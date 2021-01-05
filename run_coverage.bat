e1f408867675bca1cab279aca82c0f4cd2b13fed
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2021-01-05 17:43:59 +0100

    Added Coverage bat and sh files

---
+++ run_coverage.bat
@@ -0,0 +1,8 @@
+rmdir /q /s Artifacts
+
+SET exclude=\"[TestWeb]TestWeb.ConfigureServicesExtensions,[TestWeb]TestWeb.Program,[TestWeb]TestWeb.Startup,[*]TestWeb.Common.*,[*]TestWeb.Extensions.*,[*]TestWeb.Pages.*,[*]TestWeb.Swagger.*,[*]TestWeb.Views.*,[*]TestWeb.UnitTests.*,[*]TestWeb.Services.*,[*]Testing.Common.*,[*]TestWeb.UnitTests.*"
+dotnet test TestWeb/TestWeb.UnitTests/TestWeb.UnitTests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat="\"opencover,cobertura,json,lcov\"" /p:CoverletOutput=../Artifacts/Coverage/ /p:MergeWith='../Artifacts/Coverage/coverage.json' /p:Exclude="${exclude}"
+
+reportgenerator -reports:Artifacts/Coverage/coverage.opencover.xml -targetDir:Artifacts/Coverage/Report -reporttypes:HtmlInline_AzurePipelines
+
+"Artifacts/Coverage/Report/index.htm"
