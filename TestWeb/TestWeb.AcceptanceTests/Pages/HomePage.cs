ef23343fdc4ed50b9880e0e2c5b3eac8af2ee952
Robin Weller <robin@saucecode.co>
2021-02-16 11:42:24 +0000

    VIH-5552 Update locators for new usernames

--- TestWeb/TestWeb.AcceptanceTests/Pages/HomePage.cs
+++ TestWeb/TestWeb.AcceptanceTests/Pages/HomePage.cs
@@ -6,13 +6,13 @@ namespace TestWeb.AcceptanceTests.Pages
     public static class HomePage
     {
         public static By CaseName(string caseName) => By.XPath($"//td[contains(text(),'{caseName}')]");
-        public static By ManualAllocatedUsers = By.XPath($"//td[contains(text(),'manual_')]");
-        public static By FirstManualAllocatedUser = By.XPath($"//td[contains(text(),'manual_')][1]");
+        public static readonly By ManualAllocatedUsers = By.XPath($"//div[@class='govuk-grid-column-full' and ./h1[text()='Your Allocated Users']]//tbody/tr");
+        public static readonly By FirstManualAllocatedUser = By.XPath($"//div[@class='govuk-grid-column-full' and ./h1[text()='Your Allocated Users']]//tbody/tr[1]/td[contains(@id,'username')]");
         public static By DeleteButton(Guid hearingId) => By.Id($"{hearingId:D}-delete-button");
         public static By UnallocateButton(string username) => By.Id($"{username}-unallocate-button");
         public static By ResetPasswordButton(string username) => By.Id($"{username}-reset-button");
-        public static By ResetTitle = By.Id("resetTitle");
-        public static By ResetUsername = By.Id("reset-username");
-        public static By NewPassword = By.Id("reset-password");
+        public static readonly By ResetTitle = By.Id("resetTitle");
+        public static readonly By ResetUsername = By.Id("reset-username");
+        public static readonly By NewPassword = By.Id("reset-password");
     }
 }
